c29dc228d3173ee5f7269f9584063020777106e7
Calvin Chu <cchu00@stuy.edu>
2019-10-23 11:12:49 -0400

    started assignment

---
+++ functions.c
@@ -0,0 +1,5 @@
+#include "headers.h"
+
+void print_list(struct node *);
+struct node * insert_front(struct node *, int);
+struct node * free_list(struct node *);
---
+++ headers.h
@@ -0,0 +1,14 @@
+#ifndef HEADERS_H
+#define HEADERS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#endif
+
+struct node { int i; struct node *next; };
+
+void print_list(struct node *);
+
+struct node * insert_front(struct node *, int);
+
+struct node * free_list(struct node *);
