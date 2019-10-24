6e0c0d056b45b9b70d439f1fbc961e7e5cc28458
Calvin Chu <cchu00@stuy.edu>
2019-10-24 11:04:06 -0400

    classwork

--- functions.c
+++ functions.c
@@ -1,5 +1,18 @@
 #include "headers.h"
 
-void print_list(struct node *);
-struct node * insert_front(struct node *, int);
-struct node * free_list(struct node *);
+struct node linked_list(int val) {
+  struct node l;
+  l.i = val;
+  l.next = NULL;
+}
+void print_list(struct node * p) {
+  char out[] = "[ ";
+  out += p->i + " ";
+  struct current * n = p;
+  while (n->next != NULL) {
+    out += n->i + " ";
+    n = n->next;
+  }
+}
+struct node * insert_front(struct node * p, int);
+struct node * free_list(struct node * p);
